4a68cef2e023e60d08946f9fa4208ef13e031da2
Andrew Worsley <amworsley@gmail.com>
2011-09-08 20:06:26 +1000

    Add some configuration variable definitions

--- logSerial.py
+++ logSerial.py
@@ -8,15 +8,11 @@ import select
 import optparse
 import ConfigParser
 
+verbose = False
 dfile = '/dev/ttyACM0'
-line = serial.Serial(dfile, 38400)
-line.open()
-line.flushInput()
-
 logFile = "/home/amw/remote-control.log"
-log = open(logFile, 'a', 0);
-
 timeout = 5 # timeout in seconds when we poll the temperature
+
 def CheckInput(line) :
     if line.inWaiting() <= 0:
 	return ""
@@ -34,14 +30,30 @@ def CheckStdIn():
 	line.write('t')
 
 def ReadConfig(file):
+    config = {}
     Config = ConfigParser.ConfigParser()
     if not os.access(file, os.R_OK):
-    	return
+    	return config
     Config.read(file)
     sections = Config.sections()
     for section in sections:
-	print Config.items(section)
-    	
+	if verbose:
+	    print "Section: " + section
+	for (key, val) in Config.items(section):
+		config[key] = val
+		if verbose:
+		    print "config[" + key + "] = '" + val + "'"
+    return config
+
+def init(c):
+    global dfile, logFile, timeout
+    if 'dfile' in c:
+	dfile = c['dfile']
+    if 'logFile' in c:
+	logFile = c['logFile']
+    if 'timeout' in c:
+	timeout = c['timeout']
+
 def main():
     p = optparse.OptionParser()
     p.add_option("--verbose", "-v", action="store_true",help="enable debugging",
@@ -50,8 +62,17 @@ def main():
          default=".logSerial")
     options, args = p.parse_args()
     if options.verbose:
+	global verbose
+	verbose = options.verbose
 	print("logging output from '" + dfile + "' to log file: " + logFile + "\n")
-    ReadConfig(options.config)
+
+    init(ReadConfig(options.config))
+
+    line = serial.Serial(dfile, 38400)
+    line.open()
+    line.flushInput()
+    log = open(logFile, 'a', 0);
+
     while 1:
 	a, b, c = select.select([line], [], [], timeout)
 	if line in a:
